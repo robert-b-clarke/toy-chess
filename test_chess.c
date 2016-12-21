4926661d350f37065d406e15446aff197c7fe44e
Rob Clarke <robert.b.clarke@gmail.com>
2016-12-21 08:28:14 +0000

    Tests for Knight attacks
    Tests for knight attacks, and also helper for mapping square enum to a
    bitboard with only that piece on it

--- test_chess.c
+++ test_chess.c
@@ -4,24 +4,27 @@
 
 /* homebrew unit test stuff */
 int assert_board_eq(uint64_t a, uint64_t b, const char *message);
-int test_king_attacks();
-int test_rook_attacks();
+uint64_t sq_map(int location);
+void test_king_attacks();
+void test_rook_attacks();
+void test_knight_attacks();
 
 
 int main()
 {
     test_king_attacks();
     test_rook_attacks();
+    test_knight_attacks();
     return 0;
 }
 
 
-int test_king_attacks()
+void test_king_attacks()
 {
-    uint64_t kings = SQUARE_0 >> e4;
-    uint64_t attacks = (SQUARE_0 >> d3) | (SQUARE_0 >> d4) | (SQUARE_0 >> d5);
-    attacks |= (SQUARE_0 >> f3) | (SQUARE_0 >> f4) | (SQUARE_0 >> f5);
-    attacks |= (SQUARE_0 >> e3) | (SQUARE_0 >> e5); 
+    uint64_t kings = sq_map(e4);
+    uint64_t attacks = sq_map(d3) | sq_map(d4) | sq_map(d5);
+    attacks |= sq_map(f3) | sq_map(f4) | sq_map(f5);
+    attacks |= sq_map(e3) | sq_map(e5); 
     assert_board_eq(
         king_attacks(kings, EMPTY_BOARD), 
         attacks, 
@@ -30,15 +33,28 @@ int test_king_attacks()
 }
 
 
-int test_rook_attacks()
+void test_rook_attacks()
 {
-    uint64_t rooks = SQUARE_0 >> e4;
+    uint64_t rooks = sq_map(e4);
     uint64_t attacks = (FILE_A >> 4) | (RANK_8 << 32);
     attacks &= ~rooks;
     assert_board_eq(
         rook_attacks(rooks, EMPTY_BOARD, EMPTY_BOARD), 
         attacks, 
         "expected rook attacks in open play"
+    );
+}
+
+
+void test_knight_attacks()
+{
+    uint64_t knights = sq_map(e4);
+    uint64_t attacks = sq_map(f6) | sq_map(g5) | sq_map(g3) | sq_map(f2);
+    attacks |= sq_map(d2) | sq_map(c3) | sq_map(c5) | sq_map(d6);
+    assert_board_eq(
+        knight_attacks(knights, EMPTY_BOARD), 
+        attacks, 
+        "expected knight attacks in open play"
     );
 }
 
@@ -52,4 +68,14 @@ int assert_board_eq(uint64_t a, uint64_t b, const char *message)
         exit(1);
     }
     return 1;
+}
+
+
+uint64_t sq_map(int location)
+{
+    /* Convenience for returning a bitboard with a single piece on it
+     * could be replaced with a lookup table
+     */
+
+    return SQUARE_0 >> location;
 }
