616084663d89105f098c61f997be7e376a8c74eb
Rob Clarke <robert.b.clarke@gmail.com>
2016-12-21 18:10:09 +0000

    Tests for queen
    Test movements around friendly and enemy pieces

--- test_chess.c
+++ test_chess.c
@@ -8,6 +8,7 @@ uint64_t sq_map(int location);
 void test_king_attacks();
 void test_rook_attacks();
 void test_knight_attacks();
+void test_queen_collisions();
 
 
 int main()
@@ -15,6 +16,7 @@ int main()
     test_king_attacks();
     test_rook_attacks();
     test_knight_attacks();
+    test_queen_collisions();
     return 0;
 }
 
@@ -58,6 +60,25 @@ void test_knight_attacks()
     );
 }
 
+
+void test_queen_collisions()
+{
+    uint64_t queens = sq_map(b2);
+    uint64_t allies = sq_map(a3) | sq_map(b3) | sq_map(c3);
+    uint64_t enemies = sq_map(d1) | sq_map(d2) | sq_map(d3);
+    /* queen should be able to attack up to borders with allies and overlap
+     * with enemies
+     */
+    uint64_t attacks = sq_map(a1) | sq_map(b1) | sq_map(c1);
+    attacks |= sq_map(a2) | sq_map(c2) | sq_map(d2);
+    assert_board_eq(
+        queen_attacks(queens, enemies, allies),
+        attacks,
+        "expected queen attacks when boxed in"
+    );
+}
+
+
 int assert_board_eq(uint64_t a, uint64_t b, const char *message)
 {
     /* compare 2 64 bit bitboard layers and fail noisily if they don't
@@ -65,6 +86,7 @@ int assert_board_eq(uint64_t a, uint64_t b, const char *message)
      */
     if (a != b) {
         fprintf(stderr, "FAIL: %s\n", message);
+        printf("%d differences between boards\n", population_count(b ^ a));
         exit(1);
     }
     return 1;
