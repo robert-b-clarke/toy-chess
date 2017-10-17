4ec2ec2a741fb238d03bfae29b703a33927bc2eb
Rob Clarke <robert.b.clarke@gmail.com>
2017-10-17 21:37:42 +0100

    Revised enum declaration

--- toychess.h
+++ toychess.h
@@ -11,7 +11,7 @@
 
 #define START_POS_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
-enum squares {
+typedef enum {
     a1, b1, c1, d1, e1, f1, g1, h1,
     a2, b2, c2, d2, e2, f2, g2, h2,
     a3, b3, c3, d3, e3, f3, g3, h3,
@@ -20,7 +20,8 @@ enum squares {
     a6, b6, c6, d6, e6, f6, g6, h6,
     a7, b7, c7, d7, e7, f7, g7, h7,
     a8, b8, c8, d8, e8, f8, g8, h8
-};
+} squares;
+
 extern const char *SQUARE_NAMES[];
 
 typedef struct {
