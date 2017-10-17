bb88bdbaa676a54ab5a1bcf68be469e3cc4f8f0d
Rob Clarke <robert.b.clarke@gmail.com>
2017-10-17 21:35:23 +0100

    Move SQUARE_NAMES to source file
    Doesn't belong in the header

--- toychess.c
+++ toychess.c
@@ -22,6 +22,17 @@ const uint64_t EMPTY_BOARD = (uint64_t)0x0000000000000000;
 const uint64_t FULL_BOARD = (uint64_t)0xFFFFFFFFFFFFFFFF;
 const uint64_t WHITE_SQUARES = (uint64_t)0x55AA55AA55AA55AA;
 
+const char  *SQUARE_NAMES[] = {
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
+};
+
 
 void print_board(Bitboard board)
 {
